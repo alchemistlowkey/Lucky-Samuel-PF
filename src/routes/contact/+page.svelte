892e7cda21f5fc00985b8ccf3bedb00cf41b799c
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-07 22:43:33 +0100

    contact update

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -12,7 +12,7 @@
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 
-		const response = await fetch('http://localhost:5000/api/contact', {
+		const response = await fetch('https://portfolio-backend-virid-phi.vercel.app/api/contact', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
